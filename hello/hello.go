8dea0f80785236a6c3f4d87e4dfa658abf58dd40
snrsw <ale.nnn.r@gmail.com>
2024-04-13 05:08:39 +0000

    add: Create a module

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,28 @@
 package main
 
 import (
-	"fmt"
+    "fmt"
+    "log"
 
-	"rsc.io/quote"
+    "example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
+
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
+    if err != nil {
+        log.Fatal(err)
+    }
+    // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
